2bcba38e77e7280ba2ae9f8aea4178a066545ac5
felipedeveloper24 <felipe.developer24@gmail.com>
2023-02-27 17:25:43 -0300

    registro responsivo

--- src/pages/registro_trabajadores/registro_trabajadores.jsx
+++ src/pages/registro_trabajadores/registro_trabajadores.jsx
@@ -5,7 +5,7 @@ import Sidebar from "../../components/sidebar/sidebar";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import {useForm} from "react-hook-form";
-import { Alert } from "@mui/material";
+import { Alert, Box, Button, Card, Grid, TextField, Typography } from "@mui/material";
 
 const Registro = ()=>{
     const BASE_API = "https://intra-atrasos.cl/api";
@@ -13,7 +13,8 @@ const Registro = ()=>{
     const { register, handleSubmit, formState: { errors } } = useForm();
     const navigate = useNavigate();
 
-    const store = async(data) =>{
+    const onSubmit = async(data) =>{
+        console.log(data);
         
         let respuesta = confirm("¿Estás seguro/a de todos los datos?")
         
@@ -30,67 +31,77 @@ const Registro = ()=>{
            
             navigate("/admin");
         }
+        
     };
 
     return(
-        <div className="main-container">
+        <Grid sx={{
+            width:"100%",
+            display:"flex",
+            flexDirection:"column",
+        }}>
             <Header/>
-            <div className="w-75 mt-5">
-                <form className="row form p-3 w-75 m-auto border d-flex" method="POST" onSubmit={handleSubmit(store)} >
-                    <h1 className="h1">Registro de trabajador</h1>
-                    <div className="col-5 mb-3">
-                        <label>Rut</label>
-                        <input type="text" placeholder="xx.xxx.xxx-x" className="form-control" onChange={(e)=> setRut(e.target.value)} 
-                            {...register("rut",{
-                                required: true
-                            })}
-                        />
-                        {errors.rut && <Alert className="mt-2" severity="error">Este campo es requerido</Alert> }
-                    </div>
-                    <div className="col-5 mb-3">
-                        <label>Nombre</label>
-                        <input type="text" placeholder="Juan"  className="form-control" 
-                            {...register("nombre",{
+            <Grid sx={{
+                width:"80%",
+                display:"flex",
+                flexDirection:"column",
+                margin:"0px auto",
+                justifyContent:"center",
+                alignItems:"center",
+                marginTop:"15px"
+            }}>
+                <Typography    xs={{ variant: 'h5', component: 'h5' }}
+                sm={{ variant: 'h3', component: 'h3' }} md={{variant:"h1",component:'h1'}} lg={{variant:"h1",component:'h1'}} xl={{variant:"h1",component:'h1'}} >Registro de trabajador</Typography>
+                <Card sx={{
+                        width:"100%",
+                        minHeight:"400px",
+                        padding:"15px"
+                    }}>
+                <form onSubmit={handleSubmit(onSubmit)} style={{width:"100%",margin:"0px auto",marginLeft:"10px",display:"flex",marginTop:"10px",alignItems:"center",flexWrap:"wrap"}} method="POST" >
+                    <Grid container spacing={2}>
+                        <Grid item xs={11} xl={6} lg={6} md={6} sm={10}>
+                            <TextField label="Rut" {...register("rut",{
+                                required:"true"
+                            })} fullWidth/>
+                             {errors.rut && <Alert className="mt-2" severity="error">Este campo es requerido</Alert> }
+                        </Grid>
+                        <Grid item xs={11} xl={6} lg={6} md={6} sm={10}>
+                            <TextField  label="Nombre" fullWidth {...register("nombre",{
                                 required:true
-                            })}
-                        />
-                        {errors.nombre && <Alert className="mt-2" severity="error">Este campo es requerido</Alert> }
-                    </div>
-                    <div className="col-5 mb-3">
-                        <label>Apellido</label>
-                        <input type="text" placeholder="Pérez" className="form-control"
-                            {...register("apellido",{
-                                required: true,
-                            })}
-                        />
-                        {errors.apellido && <Alert className="mt-2" severity="error">Este campo es requerido</Alert> }
-
-                    </div>
-                    <div className="col-5 mb-3">
-                        <label>Correo</label>
-                        <input type="email" placeholder="juanperez@xxxx" className="form-control" 
-                            {...register("correo",{
-                                required: true,
-                            })}
-                        />
-                        {errors.correo && <Alert className="mt-2" severity="error">Este campo es requerido</Alert>}
-                    </div>
-                    <div className="col-5 mb-3">
-                        <label>Teléfono</label>
-                        <input type="number" placeholder="9xxxxxxxx" className="form-control" 
-                            {...register("telefono",{
+                            })} />
+                            {errors.nombre && <Alert className="mt-2" severity="error">Este campo es requerido</Alert> }
+                        </Grid>
+                        <Grid item xs={11} xl={6} lg={6} md={6} sm={10}>
+                            <TextField  label="Apellido" fullWidth {...register("apellido",{
                                 required:true
-                            })}
-                        />
-                        {errors.telefono && <Alert className="mt-2" severity="error">Este campo es requerido</Alert>}
-                    </div>
-                    <div className="col-5">
-                        <input className="w-100 btn btn-primary" type="submit" value="Enviar datos" />
-                    </div>
+                            })} />
+                            {errors.apellido && <Alert className="mt-2" severity="error">Este campo es requerido</Alert> }
+                        </Grid>
+                        <Grid item xs={11} xl={6} lg={6} md={6} sm={10}>
+                            <TextField  type="email" label="Correo"
+                                 {...register("correo",{
+                                    required: true,
+                                })}
+                            fullWidth/>
+                             {errors.correo && <Alert className="mt-2" severity="error">Este campo es requerido</Alert>}
+                        </Grid>
+                        <Grid item xs={11} xl={6} lg={6} md={6} sm={10}>
+                            <TextField  label="Teléfono"  {...register("telefono",{
+                                required:true
+                            })} fullWidth/>
+                            {errors.telefono && <Alert className="mt-2" severity="error">Este campo es requerido</Alert>}
+                        </Grid>
+                        
+                        <Grid sx={{width:"70%",margin:"0px auto",marginTop:"15px",display:"flex",justifyContent:"center"}}>
+                            <Button sx={{width:"70%"}} type="submit" variant="contained">Enviar Datos</Button>
+                        </Grid>
+                    </Grid>
                 </form>
-            </div>
+                </Card>
+            </Grid>
             
-        </div>
+       
+        </Grid>
     )
 }
 
